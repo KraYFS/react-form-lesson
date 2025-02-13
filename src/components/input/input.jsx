71f2ce1dd51bc74e906fa7d97a8eaca723c1cc6e
KraYFS <grigorievdaniil13@gmail.com>
2025-02-13 09:54:39 +0200

    updated input props

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,12 +1,20 @@
 import PropTypes from "prop-types";
 import styles from "./input.module.scss";
 
-const Input = ({ placeholder, type, register, name, ...rest }) => {
+const Input = ({
+  placeholder,
+  type,
+  register,
+  name,
+  autoComplete,
+  ...rest
+}) => {
   return (
     <input
       placeholder={placeholder}
       className={styles.input}
       type={type}
+      autoComplete={autoComplete}
       {...register(name)}
       {...rest}
     />
@@ -19,7 +27,6 @@ Input.propTypes = {
   name: PropTypes.string,
   autoComplete: PropTypes.string,
   register: PropTypes.object,
-  required: PropTypes.boolean,
 };
 
 export default Input;
